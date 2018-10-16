4aac7868191b9adb9c9f65997d65ddbfa6ba2cd3
nullspaceops <nullspaceops@mailbox.org>
2018-10-16 21:48:40 +0200

    Initial commit

---
+++ AutoArc/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.IO;
+
+namespace AutoArc
+{
+    public class Program
+    {
+        public const string OldSuffix = ".old";
+        public const string ChecksumSuffix = ".md5sum";
+
+        public readonly static string[] ArcURI = { "https://www.deltaconnected.com/arcdps/x64/", "https://www.deltaconnected.com/arcdps/x64/buildtemplates/", "https://www.deltaconnected.com/arcdps/x64/extras/", "http://martionlabs.com/wp-content/uploads/" };
+        public readonly static string[] ArcNames = { "d3d9.dll", "d3d9_arcdps_buildtemplates.dll", "d3d9_arcdps_extras.dll", "d3d9_arcdps_mechanics.dll" };
+
+        //  Custom Path
+        //private const string LocalPath = "C:/Spiele/Guild Wars 2/bin64/";
+        public const string LocalPath = "bin64/";
+        public readonly static string[] GameExeName = {"Gw2", "Gw2-64"};
+
+
+        private static void Main(string[] args)
+        {
+            foreach(string Proc in GameExeName)
+            {
+                KillGame(Proc);
+            }
+
+            foreach(string ArcDll in ArcNames)
+            {
+                Remove(LocalPath, ArcDll);
+            }
+
+            for (int k = 0; k < ArcURI.Length;) 
+            {
+                Download(ArcURI[k], LocalPath, ArcNames[k], false);
+                k += 1;
+            }
+        }
+
+        private static void Download(string remotePath, string localPath, string name, bool hasChecksum)
+        {
+
+            Console.WriteLine("Checking for {0} updates...", name);
+
+            {
+                HttpWebResponse resp = (HttpWebResponse)WebRequest.Create(remotePath + name).GetResponse();
+               
+
+                if (File.Exists(localPath + name) && File.GetLastWriteTimeUtc(localPath + name) >= resp.LastModified)
+                {
+                    Console.WriteLine("Up to date ({0}).", resp.LastModified);
+                    return;
+                }
+
+                Console.WriteLine("Downloading update ({0})...", resp.LastModified);
+                resp.Close();
+            }
+
+            if (File.Exists(localPath + name))
+                File.Copy(localPath + name, localPath + name + OldSuffix, true);
+
+            byte[] file;
+            using (WebClient wc = new WebClient())
+            {
+                file = wc.DownloadData(remotePath + name);
+
+                if (hasChecksum)
+                {
+                    using (MD5 md5 = MD5.Create())
+                    {
+                        string checksum = wc.DownloadString(remotePath + name + ChecksumSuffix).Split(' ')[0];
+                        string downloadChecksum = BitConverter.ToString(md5.ComputeHash(file)).Replace("-", "").ToLower();
+                        bool equal = checksum == downloadChecksum;
+                        Console.WriteLine("{0} == {1} ({2})", checksum, downloadChecksum, equal);
+                        if (!equal)
+                        {
+                            Console.ReadKey();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No checksum verification.");
+                }
+                wc.CancelAsync();
+            }
+            
+
+            File.WriteAllBytes(localPath + name, file);
+        }
+        private static void Remove(string LocalPath, string name)
+        {
+            // Need it to remove all files to download "d3d9_arcdps_extras.dll" properly?...
+            System.IO.File.Delete(LocalPath + name);
+            Console.WriteLine("Removing {0}...", name);
+        }
+
+        private static void KillGame(string GameExeName)
+        {
+            foreach (Process process in Process.GetProcessesByName(GameExeName))
+            {
+                process.Kill();
+                Console.WriteLine("Sleeping...");
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine("################");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+This is a simple updater for ArcDPS, an unofficial Guild Wars 2 plugin.
+
+This version of the updater downloads/updates:
+- The main module (DPS Meter)
+- The buildtemplates module (Templates)
+- The mechanics module (Mechanics Log & Chart)
+- The extra module (Hide UI Stuff)
+
+You access them ingame - check https://www.deltaconnected.com/arcdps/ for more info.
+
+Drop this file in your GW2 folder and you can simply update all modules at once, create a shortcut for usage.
+
+When you start it, it will kill off any instances of Gw2, so be careful.
+
+Feel free to compile yourself, if you don't trust me for any reason.
+
+
+------------------------------------------------------------------------
+*DISCLAIMER:*
+
+The author, contributors, administrators, vandals or anyone else connected to nullspaceops assumes no responsibility or liability for any errors or omissions in the content of this site including but not limited to the documents, information, applications, codes and inter alia. The information contained in this site is provided on an “as is” basis with no guarantees of completeness, accuracy, usefulness or timeliness. Any action you take upon the information, documents, applications, codes and inter alia on this webpage is strictly at your own risk, and we will not be liable for any losses or damages in connection with the use of such.
